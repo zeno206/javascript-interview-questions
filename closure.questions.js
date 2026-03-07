f051e13ace072bb833c0bec60fdfe64f7732f7ab
shubhamdhiman <shubham.dhiman.mehra@gmail.com>
2026-03-07 09:00:05 +0530

    first commit

---
+++ closure.questions.js
@@ -0,0 +1,16 @@
+//Q-1
+//What is the output? ( How make it correct )
+
+for (var i = 0; i <= 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 1000);
+}
+
+//output
+6;
+6;
+6;
+6;
+6;
+6;
