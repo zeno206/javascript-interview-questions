03edf2a3feb1b96e0299a789acc6ebfb9cc5def8
shubhamdhiman <shubham.dhiman.mehra@gmail.com>
2026-03-07 09:43:25 +0530

    fourth-commit

--- closure.questions.js
+++ closure.questions.js
@@ -1,5 +1,5 @@
-//Q-1
-//What is the output? ( How make it correct )
+/*//Q-1
+//What is the output? 
 
 for (var i = 0; i <= 5; i++) {
   setTimeout(function () {
@@ -8,9 +8,71 @@ for (var i = 0; i <= 5; i++) {
 }
 
 //output
-6;
-6;
-6;
-6;
-6;
-6;
+//6;
+//6;
+//6;
+//6;
+//6;
+//6;
+
+*/
+//Q-3
+
+/*var func = [];
+for (var i = 0; i < 5; i++) {
+  func[i] = function () {
+    console.log(i);
+  };
+}
+func[3]();
+
+//output
+5;*/
+
+//Q-3
+
+(function () {
+  var a = (b = 5);
+})();
+console.log(b);
+
+//output
+//nothing
+//if we chaneg the var to a = (b=5) then the output is 5
+
+//Q-4
+
+function outer() {
+  let count = 0;
+
+  return function () {
+    count++;
+    console.log(count);
+  };
+}
+
+const counter = outer();
+
+counter();
+counter();
+counter();
+//output
+//1
+//2
+//3
+
+//Q-5
+
+function multiplier(x) {
+  return function (y) {
+    return x * y;
+  };
+}
+
+const double = multiplier(2);
+const triple = multiplier(3);
+
+console.log(double(5));
+console.log(triple(5));
+//output
+//10//15
